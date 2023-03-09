eb31e78d29cd3cc77205a91394976202e5bf8a0d
Mackenzie Child <mackenzie.child@gmail.com>
2023-03-09 09:32:07 -0500

    Tied forms to state

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,9 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('');
+  const [variant, setVariant] = React.useState('notice');
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +27,12 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea 
+              id="message" 
+              className={styles.messageInput} 
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
           </div>
         </div>
 
@@ -33,18 +41,24 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
 
-            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map((variantOption, index) => (
+              <div key={variantOption}>
+                <label htmlFor={`variant-${variantOption}`}>
+                  <input 
+                    id={`variant-${variantOption}`}
+                    type="radio"
+                    name="variant"
+                    value={variantOption}
+                    checked={variant === variantOption}
+                    onChange={e => setVariant(e.target.value)}
+                  />
+                  {variantOption}
+                </label>
+              </div>
+            ))}
           </div>
+
         </div>
 
         <div className={styles.row}>
